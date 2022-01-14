69fe82abb87c698f5febaeb09ec0f54e7ef6e8c3
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-01-14 11:33:57 +0100

    test 3 y 4

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -20,6 +22,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "feedings")
 public class Feeding {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,5 +40,11 @@ public class Feeding {
 
     @NotNull
     @ManyToOne
-    Pet pet;   
+    @JoinColumn(name = "pet_id")
+    Pet pet; 
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "feeding_type_id")
+    FeedingType feedingType;    
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "feeding_types")
 public class FeedingType {
     
     @Id
