1fb6fee9b5c6b5634774290a80b700e8f481c2e3
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-01-14 10:59:24 +0100

    Test 1 correcto

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,16 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +19,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Feeding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+    
+    @NotNull
+    @Column(name = "start_date")  
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
     LocalDate startDate;
+
+    @NotNull
+    @Column(name = "weeks_duration")  
+    @Min(1)
     double weeksDuration;
+
+    @NotNull
+    @ManyToOne
     Pet pet;   
 }
