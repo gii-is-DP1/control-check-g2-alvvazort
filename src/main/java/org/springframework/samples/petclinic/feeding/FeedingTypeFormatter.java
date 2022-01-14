7b1836f7f9ac5f5bfa393b2ba63972e70fdbd4a3
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-01-14 11:50:30 +0100

    7 tests funcionando

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
@@ -3,22 +3,31 @@ package org.springframework.samples.petclinic.feeding;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedingTypeFormatter implements Formatter<FeedingType>{
 
+    @Autowired 
+    FeedingService feedingService;
+
     @Override
     public String print(FeedingType object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+
+        return object.getName();
     }
 
     @Override
     public FeedingType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+
+        for (FeedingType ft : this.feedingService.getAllFeedingTypes()){
+            if(ft.getName().equals(text))
+                return ft;
+        }
+        throw new ParseException("This is not a product type", 0);
+
     }
     
 }
